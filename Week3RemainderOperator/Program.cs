d1c6ae6c437a70a2f3a9e911c9096a572e2c60c7
Alina Hanselle <alina-hanselle@web.de>
2023-09-12 14:31:16 +0300

    week3

---
+++ Week3RemainderOperator/Program.cs
@@ -0,0 +1,31 @@
+﻿//Odd or Even
+
+int a = 10;
+
+int result = 10 % 2;
+
+Console.WriteLine(result);
+
+switch (result)
+{
+    case 0:
+        Console.WriteLine($"{a} is an even number");
+        break;
+    case 1:
+        Console.WriteLine($"{a} is an odd number");
+        break;
+    default:
+        Console.WriteLine("Invalid value")
+}
+
+
+//Old Code
+//if(result == 0)
+//{
+//    Console.WriteLine($"{a} is an even number");
+
+//}
+//else
+//{
+//    Console.WriteLine($"{a} is an odd number");
+//}
